17cc97394b05f8f3df86b60af25ca8f78dc58078
Joshua Clark <clarkjoshua301@gmail.com>
2019-03-01 21:49:23 -0500

    Update and rename Nueral netword concrete data.R to Neural network concrete data.R

--- Neural network concrete data.R
+++ Neural network concrete data.R
@@ -20,13 +20,14 @@ plot(concrete_ANNmodel)
 model_testresults <- compute(concrete_ANNmodel, concrete_data_test[1:8])
 strength_prediction <- model_testresults$net.result
 cor(strength_prediction, concrete_data_test$Concrete.compressive.strength.MPa..megapascals..)
-#strength_prediction = .698, decide to refine model further 
+#strength_prediction = .698, decide to refine model further, expect hidden layers. 
 model2 <- neuralnet(Concrete.compressive.strength.MPa..megapascals.. ~ Cement..component.1..kg.in.a.m.3.mixture. + 
                       Blast.Furnace.Slag..component.2..kg.in.a.m.3.mixture. + 
                       Water...component.4..kg.in.a.m.3.mixture. + Water...component.4..kg.in.a.m.3.mixture. + 
                       Fly.Ash..component.3..kg.in.a.m.3.mixture. + Superplasticizer..component.5..kg.in.a.m.3.mixture. + 
                       Coarse.Aggregate...component.6..kg.in.a.m.3.mixture. + Fine.Aggregate..component.7..kg.in.a.m.3.mixture. + 
                       Age..day., data=concrete_data_train, hidden=5)
+#5 hidden layers yielded the best results
 model2_results <- compute(model2, concrete_data_test[1:8])
 strength_prediction2 <- model2_results$net.result 
 cor(strength_prediction2, concrete_data_test$Concrete.compressive.strength.MPa..megapascals..)
